4e5b22dcb1d3a810faacd4fd987a1e835f04b9eb
Roman Stravnyak <350loljke@gmail.com>
2020-01-19 17:59:38 +0700

    95%

--- script.js
+++ script.js
@@ -1,133 +1,146 @@
 const page = document.querySelector('.page');
 const buyButton = document.querySelector('.popup__button_buy');
-const basketIcon = document.querySelector('.basket-icon');
-const sellout = document.querySelector('.sellout');
+const basketIcon = document.querySelector('.basket-icon__image');
 const basketData = new Array;
 
-const createBook = (image, title, subtitle, url, price, isbn) =>  {
-    const bookContainer = document.querySelector(".books-container");
-    const book = document.createElement("div");
+// *** Функция создания книги путем рендера данных с сервера (рендер) ***
 
-    bookContainer.appendChild(book);
-    book.classList.add("book");
+const createBook = ( { image, title, subtitle, url, price, isbn13 } ) =>  {
+    const book = document.createElement("div");
+    book.classList.add('book');
+    document.querySelector('.books-container').append(book);
+
     book.insertAdjacentHTML('beforeEnd', `   
         <div class="book__image"></div>
         <p class="book__title"></p>`);
 
     book.querySelector('.book__image').style.backgroundImage = `url(${ image })`;
     book.querySelector('.book__title').textContent = title;
+
     book.setAttribute('subtitle', subtitle);
     book.setAttribute('url', url);
-    book.setAttribute('isbn', isbn);
+    book.setAttribute('isbn13', isbn13);
     book.setAttribute('price', price);
-   
+
 }
 
+// *** Получение данных с сервера и последующий рендер через createBook ***
+
 const getBooksData = () => {
-    fetch ('http://5d22b7fd4e05c600146ef4dd.mockapi.io/cupcake/books')
+    fetch ( 'http://5d22b7fd4e05c600146ef4dd.mockapi.io/cupcake/books' )
         .then (res => {
             if (res.ok) return res.json();
                         return Promise.reject( `Ошибка: ${ res.status }` );  
         })
 
-        .then (json => json.books.forEach(item => {
-            createBook( item.image, item.title, item.subtitle, item.url, item.price, item.isbn13 );
-        }))
+        .then (json => json.books.forEach( item => createBook( item )) )
         .catch(err => console.log( err ));
 }
 
+// *** Создаем данные для popup-ов  ***
+
+const createPopupInfo = () => {
+    
+    const title = document.querySelector('.popup__title');
+    const image = document.querySelector('.popup__image');
+    const parent = event.target.parentElement;
+    const bgImage =  event.target.style.backgroundImage;
+
+    title.textContent = event.target.nextElementSibling.textContent;
+    image.setAttribute('src', bgImage.slice(5, bgImage.length-2));
+    image.setAttribute('alt', title.textContent);
+    
+    document.querySelector('.popup__subtitle').textContent = parent.getAttribute('subtitle');
+    document.querySelector('.popup__isbn13').textContent = parent.getAttribute('isbn13');
+    document.querySelector('.popup__price').textContent = parent.getAttribute('price');
+    document.querySelector('.popup__url').href = parent.getAttribute('url');
+
+}
+
+// *** Открываем popup при клике и заполняем контентом ***
 
 const showBookPopup = (event) => {
-    const popup = document.querySelector('.popup');
-    const title = document.querySelector('.popup__title');
-    const subtitle = document.querySelector('.popup__subtitle');
-    const image = document.querySelector('.popup__image');
-    const isbn = document.querySelector('.popup__isbn');
-    const price = document.querySelector('.popup__price');
-    const url = document.querySelector('.popup__url');
     
     if (event.target.classList.contains('book__image')) {
-        console.log(event.target);
-        popup.classList.add('popup_is-opened');
-        title.textContent = event.target.nextElementSibling.textContent;
-        image.setAttribute('src', event.target.style.backgroundImage.slice(5, event.target.style.backgroundImage.length-2));
-        
-        subtitle.textContent = event.target.parentElement.getAttribute('subtitle');
-        isbn.textContent = event.target.parentElement.getAttribute('isbn');
-        price.textContent = event.target.parentElement.getAttribute('price');
-        url.href = event.target.parentElement.getAttribute('url');
-  
+        document.querySelector('.book-popup').classList.add('popup_is-opened');
+        createPopupInfo();
     };
 }
 
+// *** Функция для выбора количества товара ***
+
 const changeVal = () => {
-    const minusVal = document.querySelector('.popup__button_minus');
-    const plusVal = document.querySelector('.popup__button_plus');
-    const input = document.querySelector('.popup__input');
-    minusVal.onclick = () => {
-            if (+input.textContent > 1) input.textContent = +input.textContent - 1
-        };
-    plusVal.onclick = () => 
-                input.textContent = +input.textContent + 1;
+    const minus = document.querySelector('.popup__button_minus');
+    const plus = document.querySelector('.popup__button_plus');
+    const counter = document.querySelector('.popup__counter');
+    minus.onclick = () => {
+            if (+counter.textContent > 1) 
+                counter.textContent = +counter.textContent - 1
+            };
+    plus.onclick = () => 
+            counter.textContent = +counter.textContent + 1;
 }
+
+// *** Закрываем все popup-ы содержащие popup__close ***
 
 const popupClose = (event) => {
     if (event.target.classList.contains('popup__close'))
     event.target.parentElement.parentElement.classList.remove('popup_is-opened');
 };
 
-const showBasket = () => {
-    const table = document.querySelector('.basket__table');
-    const thElem = document.querySelectorAll('.ggg');
-    thElem.forEach(item => item.remove());
-    console.log(thElem);
-    sellout.classList.add('popup_is-opened');
-    basketData.forEach((item, index) => {
-    table.insertAdjacentHTML('beforeEnd', ` 
-    <th scope="row" class="ggg">${ index + 1 }</th>  
-    <td class="ggg">${ item.title }</td>
-    <td class="ggg">${ item.count }</td>
-    <td class="ggg">${ item.totalPrice }</td>`);
+//
 
+const showBasket = () => {
+    document.querySelectorAll('.table__item').forEach( item => item.remove() );
+    document.querySelector('.basket-popup').classList.add('popup_is-opened');
+
+    basketData.forEach(( item, index ) => {
+    document.querySelector('.basket__table').insertAdjacentHTML('beforeEnd', ` 
+        <th scope="row" class="table__item table__counter">${ index + 1 }</th>  
+        <td class="table__item">${ item.title }</td>
+        <td class="table__item table__val">${ item.count }</td>
+        <td class="table__item table__val">${ item.totalPrice }</td>
+        `)
+       // <td class="table__item"><button class="btn btn-warning">Del</button></td>
     })
 }
 
-const selloutTabFill = () => {
+const basketContent = () => {
 
     const data = new Object;
     const currentBook = event.target.parentElement.parentElement;
+    const counter = currentBook.querySelector('.popup__counter');
 
     data.title = currentBook.querySelector('.popup__title').textContent;
-    data.count = currentBook.querySelector('.popup__input').textContent;
+    data.count = counter.textContent;
     data.totalPrice = (+currentBook.querySelector('.popup__price').textContent * +data.count).toFixed(2);
 
     const index = basketData.findIndex(item => item.title === data.title);
+
+    // *** Проверяем есть ли в массиве ранее добавленная книга ***
     if (index !== -1) {
         basketData[index].count = +basketData[index].count + +data.count;
         basketData[index].totalPrice = (+basketData[index].totalPrice + +data.totalPrice).toFixed(2);
-    }
-
-    /*if (basketData.some(item => item.title === data.title)) {
-        
-        basketData.forEach(item => {
-            if (item.title === data.title) {
-                item.count  = +item.count + +data.count;
-                item.totalPrice = (+item.totalPrice + +data.totalPrice).toFixed(2);
-            }
-        })
-    }*/ else {
+    } else 
         basketData.push(data);
-    }
-
+    
     alert('Товар добавлен в корзину!');
-    document.querySelector('.popup__input').textContent = 1;
+    basketIcon.setAttribute('src', './images/shoppingbasket_full.svg');
+    document.querySelector('.basket-icon__counter').textContent = basketData.reduce( (prev, curr) => prev + +curr.count, 0 );
+    counter.textContent = 1;
 }
 
+const removeBook = () => {
+    if (event.target.classList.contains('table__button')) {
+        console.log(event.target.parentElement.parentElement);
+        
+    };
+}
 getBooksData();
 changeVal();
 
 page.addEventListener('click', showBookPopup);
 page.addEventListener('click', popupClose);
-buyButton.addEventListener('click', selloutTabFill);
+page.addEventListener('click', removeBook);
+buyButton.addEventListener('click', basketContent);
 basketIcon.addEventListener('click', showBasket);
-
